9d8d488bc18f2b52798b2e62127567ec34364dbb
omoiseie <omoiseie@cisco.com>
2023-02-05 13:10:45 +0100

    apply prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        APP
-    </div>
-  );
+  return <div className="App">APP</div>;
 }
 
 export default App;
