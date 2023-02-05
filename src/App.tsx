54d117521eba4acc279f338ee32de0b0c3c1393c
omoiseie <omoiseie@cisco.com>
2023-02-05 20:12:34 +0100

    init Login, OrderView components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Login from './login/Login';
+import OrderView from './order-view/OrderView';
+import {OrderProvider} from "./provider/OrderProvider";
 import './App.css';
 
 function App() {
-  return <div className="App">APP</div>;
+  return (
+    <OrderProvider>
+      <Routes >
+        <Route path="/" element={<Login />} />
+        <Route path="/order-view/:id" element={<OrderView />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </OrderProvider>
+  );
 }
 
 export default App;
+
+function NotFound() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+    </div>
+  );
+}
