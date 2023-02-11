3dd0260349645cb077e84622f91a1d635fbd9b3f
omoiseie <omoiseie@cisco.com>
2023-02-11 14:26:57 +0100

    ad StyledMainLayout

---
+++ src/shared/StyledMainLayout.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledMainLayout = styled.main`
+  height: calc(100% - 50px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--main-bg-color);
+`;
+
+export default StyledMainLayout;
