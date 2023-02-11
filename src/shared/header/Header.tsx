a5f5e3f4d3bab82f10992ca3a2c5167a51a49973
omoiseie <omoiseie@cisco.com>
2023-02-11 14:26:24 +0100

    add Header component

---
+++ src/shared/header/Header.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import StyledHeader from "./StyledHeader";
+
+const Header = () => {
+  return <StyledHeader>🚀 ROCKET DELIVERY</StyledHeader>;
+};
+
+export default Header;
---
+++ src/shared/header/StyledHeader.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 24px;
+  height: 50px;
+  box-shadow: 1px 2px 3px var(--main-shadow-color);
+  z-index: 1;
+  position: relative;
+`;
+
+export default StyledHeader;
