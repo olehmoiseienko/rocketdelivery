8838175c3b36fdc2036686cde7b84626614ab4fb
omoiseie <omoiseie@cisco.com>
2023-02-12 10:52:08 +0100

    impl Heading components

---
+++ src/shared/StyledHeading.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledHeading = styled.h1`
+  font-size: var(--font-size-h1);
+  font-weight: 600;
+  color: var(--main-dark-grey-color);
+`;
+
+export default StyledHeading;
---
+++ src/shared/StyledSubHeading.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const StyledSubHeading = styled.h2`
+  margin: 8px 0;
+  font-size: var(--font-size-md);
+  font-weight: 600;
+`;
+
+export default StyledSubHeading;
